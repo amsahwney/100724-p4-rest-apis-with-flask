76761085947327d47228bc3b780c7abd47565797
Amrita <amritak.saw@gmail.com>
2024-12-10 12:05:12 -0500

    lesson complete

--- server/app.py
+++ server/app.py
@@ -2,11 +2,65 @@
 
 from flask import request
 from config import app, db
-from models import db
+from models import Landlord
 
 @app.get('/')
 def index():
     return "Hello world"
+
+#GET REQUEST TO GET ALL LANDLORDS
+@app.get("/landlords")
+def get_all_landlords():
+
+    all_landlord = Landlord.query.all()
+    landlord_dicts = [ landlord.to_dict() for landlord in all_landlord]
+    return landlord_dicts, 200
+
+    # do the same thing in one line
+    # return [ l.to_dict() for l in Landlord.query.all() ], 200
+
+#GET REQUEST TO GET LANDLORD BY ID
+@app.get("/landlords/<int:landlord_id>")
+def get_landlord_by_id(landlord_id):
+    # 1. find the landlord with that id
+    found_landlord = Landlord.query.where(Landlord.id == landlord_id).first()
+
+    if found_landlord:
+        # 2. send it to the client if it exists
+        return found_landlord.to_dict(), 200
+    else:
+        # 3. send a 404 if it doesn't exist
+        return { "status": 404, "message": "Not Found" }, 404 
+
+
+
+#POST REQUEST TO MAKE A NEW LANDLORD
+@app.post("/landlords")
+def create_new_landlords(): 
+    # 1. parse the body
+    body = request.json
+
+    try:
+        # 2. create a new landlord instance using the parsed body
+        new_landlord = Landlord(
+            associated_llcs = body.get('associated_llcs'), 
+            violations = body.get('violations'), 
+            currently_in_litigation = body.get('currently_in_litigation'))
+        
+        # 3. add and commit the new landlord 
+        db.session.add( new_landlord )
+        db.session.commit()
+
+        # 4. transform landlord into a dictionary and return the dictionary in the response
+        return new_landlord.to_dict(), 201
+    
+    # 5. return an error message for errors
+    except Exception as error:
+        return{ 
+            "status": 400,
+            "message": "something went wrong... do you have an associated_llc", 
+            "error_text": str(error)
+        }, 400
 
 
 # RUN ##########################
--- server/models.py
+++ server/models.py
@@ -2,4 +2,18 @@
 # from sqlalchemy_serializer import SerializerMixin
 from config import db
 
-# MODELS GO HERE
+class Landlord(db.Model):
+    __tablename__ = 'landlords_table'  
+
+    id = db.Column(db.Integer, primary_key=True)
+    associated_llcs = db.Column(db.String, nullable=False)
+    violations = db.Column(db.String)
+    currently_in_litigation = db.Column(db.Boolean, default=True)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "associated_llcs": self.associated_llcs,
+            "violations": self.violations, 
+            "currently_in_litigation": self.currently_in_litigation
+        }
